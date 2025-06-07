50e54fd76e31de0dd7465aa38916f5c93fcfadc8
Ngetich-86 <gideonkngetich86@gmail.com>
2025-06-07 09:00:55 -0400

    updated the routes

--- Backend/backend/server/urls.py
+++ Backend/backend/server/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('categories/<int:pk>/', views.category_detail, name='api_category_detail'),
     path('products/', views.get_products, name='get_products'),
     path('products/<int:pk>/', views.product_detail, name='api_product_detail'),
+    path('suppliers/', views.get_suppliers, name='get_suppliers'),
+    path('suppliers/<int:pk>/', views.supplier_detail, name='api_supplier_detail'),
+    path('payments/', views.get_payments, name='get_payments'),
+    path('payments/<int:pk>/', views.payment_detail, name='api_payment_detail'),
+    # path('payments/<int:pk>/product/', views.payment_product_detail, name='api_payment_product_detail'),
 ]
